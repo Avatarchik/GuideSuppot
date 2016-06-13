fae992c4b1a5cab5a44b7d08c6a8f75034961ba1
uribou <yuta.wakabayashi.o@gmail.com>
2016-06-14 04:33:27 +0900

    いろいろ

---
+++ Assets/Scripts/Egocentric/RotateCamera.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class RotateCamera : MonoBehaviour
+{
+    private Quaternion start_gyro;
+    private Quaternion initialRotation; 
+    private long _lastOscTimeStamp = -1;
+
+    void Awake()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    void Start()
+    {
+        OSCHandler.Instance.Init("", 0, 12000);
+        initialRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        OSCHandler.Instance.UpdateLogs();
+        foreach (KeyValuePair<string, ServerLog> item in OSCHandler.Instance.Servers)
+        {
+            for (int i = 0; i < item.Value.packets.Count; i++)
+            {
+                if (_lastOscTimeStamp < item.Value.packets[i].TimeStamp)
+                {
+                    _lastOscTimeStamp = item.Value.packets[i].TimeStamp;
+
+                    string address = (string)item.Value.packets[i].Data[0];
+                    string[] data = address.Split(',');
+                    Quaternion a = new Quaternion(
+                        float.Parse(data[0]),
+                        float.Parse(data[1]),
+                        float.Parse(data[2]),
+                        float.Parse(data[3])
+                    );
+                    if (start_gyro.x == 0.0)
+                    {
+                        Quaternion b = Quaternion.Euler(0, 0, 90) * a;
+                        start_gyro = b;
+                    }
+                    else
+                    {
+                        Quaternion b = Quaternion.Inverse(start_gyro) * Quaternion.Euler(0, 0, 90) * a;
+                        this.transform.localRotation = new Quaternion(-b.x, -b.y, -b.z, b.w);
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/GyroSender.cs
+++ Assets/Scripts/GyroSender.cs
@@ -1,35 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class GyroSender : MonoBehaviour
 {
-    private Quaternion formattedGyro;
-    public string toIP = "0.0.0.0";
+    public string toIP = "192.168.10.6";
+    private long _lastOscTimeStamp = -1;
 
     void Awake()
     {
         Input.gyro.enabled = true;
-        formatteGyro();
     }
 
     void Start()
     {
+        OSCHandler.Instance.Init("192.168.10.8", 12000, 0);
+        sendGyro();
     }
 
     void Update()
     {
+        OSCHandler.Instance.UpdateLogs();
+        foreach (KeyValuePair<string, ServerLog> item in OSCHandler.Instance.Servers)
+        {
+            for (int i = 0; i < item.Value.packets.Count; i++)
+            {
+                if (_lastOscTimeStamp < item.Value.packets[i].TimeStamp)
+                {
+                    _lastOscTimeStamp = item.Value.packets[i].TimeStamp;
+
+                    string address = item.Value.packets[i].Address;
+
+                    Debug.Log(address);
+                }
+            }
+        }
+
         formatteGyro();
+        sendGyro();
     }
 
     void formatteGyro()
     {
         Quaternion gyro = Input.gyro.attitude;
-        formattedGyro = Quaternion.Euler(90, 0, 0) * (new Quaternion(-gyro.x, -gyro.y, gyro.z, gyro.w));
+        //formattedGyro = Quaternion.Euler(90, 0, 0) * (new Quaternion(-gyro.x, -gyro.y, gyro.z, gyro.w));
     }
 
     void sendGyro()
     {
-
+        Quaternion gyro = Input.gyro.attitude;
+        string mes = gyro.x.ToString() + ',' +
+                     gyro.y.ToString() + ',' +
+                     gyro.z.ToString() + ',' +
+                     gyro.w.ToString();
+        OSCHandler.Instance.SendMessageToClient("myRemoteLocation", toIP + ":12000", mes);
     }
 }
 
